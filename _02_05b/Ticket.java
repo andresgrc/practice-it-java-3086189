e19c8e0460cff2a02b6f95a7b96fa0519451c9a5
Andres Garcia Sanchez <104814580+andresgrc@users.noreply.github.com>
2026-01-28 02:48:59 +0000

    _02_05b Activity

--- _02_05b/Ticket.java
+++ _02_05b/Ticket.java
@@ -10,10 +10,32 @@ public class Ticket {
   private double price;
   private boolean isReturn;
 
-  // Add three public methods to set the value of each field, called
-  // setDestination, setPrice and setIsReturn.
+  // Add three methods to set the value of each field, called setDestination,
+  // setPrice and setIsReturn.
+  public void setDestination(String destination) {
+    this.destination = destination;
+  }
 
-  // Add three public methods to get the value of each field, called
-  // getDestination, getPrice and getIsReturn.
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  public void setIsReturn(boolean isReturn) {
+    this.isReturn = isReturn;
+  }
+
+  // Add a separate method to get the value of each field, called getDestination,
+  // getPrice and getIsReturn.
+  public String getDestination() {
+    return destination;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public boolean getIsreturn() {
+    return isReturn;
+  }
 
 }
